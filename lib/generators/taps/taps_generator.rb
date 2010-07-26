d2176646de026dbb762e377ce6c6660ea8e54975
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2010-07-26 12:07:58 -0500

    don't break rails 2 generate

--- lib/generators/taps/taps_generator.rb
+++ lib/generators/taps/taps_generator.rb
@@ -1,15 +1,17 @@
-class TapsGenerator < Rails::Generators::Base # :nodoc:
-  source_root File.expand_path('../templates', __FILE__)
-  add_shebang_option!
+if defined?(Rails::Generators::Base)
+  class TapsGenerator < Rails::Generators::Base # :nodoc:
+    source_root File.expand_path('../templates', __FILE__)
+    add_shebang_option!
   
-  def script
-    directory "script" do |content|
-      "#{shebang}\n" + content
+    def script
+      directory "script" do |content|
+        "#{shebang}\n" + content
+      end
+      chmod "script", 0755, :verbose => false
     end
-    chmod "script", 0755, :verbose => false
-  end
   
-  def gemfile
-    gem 'taps', '~>0.3', :group => 'development'
+    def gemfile
+      gem 'taps', '~>0.3', :group => 'development'
+    end
   end
 end
